6860a78bfd49447fcd2e091f881ad196b17a1ade
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-08-18 22:50:58 +0000

    commit

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,5 +1,15 @@
 import 'dotenv/config';
+import express from "express";
 
 
 console.log("hello world!!!");
 console.log(process.env.MY_SECRET);
+
+console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => {console.log("Example app listening")});
