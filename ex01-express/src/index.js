fec3dee5504c4824f6672a14533d328ab9da90d3
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-08-25 22:51:57 +0000

    adicionando requisicoes

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,15 +1,26 @@
 import 'dotenv/config';
+import cors from "cors";
 import express from "express";
 
 
 console.log("hello world!!!");
-console.log(process.env.MY_SECRET);
+console.log("MY_SECRET", process.env.MY_SECRET);
 
 console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
 const app = express();
 
+app.use(cors());
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 app.get("/", (req, res) => {
     res.send("Bem-vindo ao Express de Pedro");
 });
+
+app.get("/example", (req, res) => {
+    res.send("teste");
+});
+
 
 app.listen(3000, () => {console.log("Example app listening")});
