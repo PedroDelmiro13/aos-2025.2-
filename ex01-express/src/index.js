b5e62cc7d13074c49635f18700d4158c487c31a2
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-09-01 22:46:31 +0000

    pegando conteudo da aula passada

--- ex01-express/src/index.js
+++ ex01-express/src/index.js
@@ -1,26 +1,44 @@
-import 'dotenv/config';
+import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
-
-console.log("hello world!!!");
+console.log("Olá, Turma!");
 console.log("MY_SECRET", process.env.MY_SECRET);
-
 console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
+
 const app = express();
+app.set("trust proxy", true);
 
-app.use(cors());
+var corsOptions = {
+  origin: ["http://example.com", "*"],
+  optionsSuccessStatus: 200, 
+};
+app.use(cors(corsOptions));
+
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-})
+      console.log(`${req.method} ${req.path} - ${req.ip}`);
+        next();
+        });
+
+
 app.get("/", (req, res) => {
-    res.send("Bem-vindo ao Express de Pedro");
+  return res.send("Received a GET HTTP method");
 });
 
-app.get("/example", (req, res) => {
-    res.send("teste");
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
 });
 
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
 
-app.listen(3000, () => {console.log("Example app listening")});
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
